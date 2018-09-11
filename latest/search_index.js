b530cd86c54b8958149600ea30ffdd3078eec355
autodocs <autodocs>
2018-09-11 12:17:30 +0000

    build based on 40ef3d5

--- latest/search_index.js
+++ latest/search_index.js
@@ -371,21 +371,29 @@ var documenterSearchIndex = {"docs": [
 {
     "location": "YearBook/index.html#-7",
     "page": "历史年鉴",
+    "title": "1988",
+    "category": "section",
+    "text": "9.17-10.2 汉城举办第二十四届奥林匹克运动会"
+},
+
+{
+    "location": "YearBook/index.html#-8",
+    "page": "历史年鉴",
     "title": "1991",
     "category": "section",
     "text": "8.19-21 苏共保守派发动政变，于黑海畔软禁戈尔巴乔夫\n8.24 戈尔巴乔夫辞去苏共总书记\n12.25 苏联总统戈尔巴乔夫辞职\n12.26 苏联最高苏维埃通过决议宣布解体"
 },
 
 {
-    "location": "YearBook/index.html#-8",
+    "location": "YearBook/index.html#-9",
     "page": "历史年鉴",
     "title": "2001",
     "category": "section",
-    "text": "9.11 九一一事件，基地组织恐怖分子劫持四架飞机分别撞向世贸大厦、五角大楼、国会大厦（未成功）。计造成2977人死亡或失踪\n10.7 美国联军对阿富汗基地组织和塔利班发起反恐战争"
+    "text": "7.13 在莫斯科举行的奥林匹克委员会112届年会上，北京取得2008年奥运会举办权\n9.11 九一一事件，基地组织恐怖分子劫持四架飞机分别撞向世贸大厦、五角大楼、国会大厦（未成功）。计造成2977人死亡或失踪\n10.7 美国联军对阿富汗基地组织和塔利班发起反恐战争"
 },
 
 {
-    "location": "YearBook/index.html#-9",
+    "location": "YearBook/index.html#-10",
     "page": "历史年鉴",
     "title": "2003",
     "category": "section",
@@ -393,7 +401,15 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "YearBook/index.html#-10",
+    "location": "YearBook/index.html#-11",
+    "page": "历史年鉴",
+    "title": "2004",
+    "category": "section",
+    "text": "9.1-9.3 别斯兰人质事件。车臣武装分子在北奥塞梯共和国别斯兰市一所中学，劫持1200人为人质。最终造成333名人质死亡"
+},
+
+{
+    "location": "YearBook/index.html#-12",
     "page": "历史年鉴",
     "title": "2006",
     "category": "section",
@@ -401,7 +417,7 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "YearBook/index.html#-11",
+    "location": "YearBook/index.html#-13",
     "page": "历史年鉴",
     "title": "2007",
     "category": "section",
@@ -409,7 +425,15 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "YearBook/index.html#-12",
+    "location": "YearBook/index.html#-14",
+    "page": "历史年鉴",
+    "title": "2008",
+    "category": "section",
+    "text": "5.12 下午2：28汶川发生8级地震，共计（9.18）造成69227人死亡，374643人受伤，17923人失踪\n8.8-24 北京举办第二十九届奥林匹克运动会"
+},
+
+{
+    "location": "YearBook/index.html#-15",
     "page": "历史年鉴",
     "title": "2011",
     "category": "section",
@@ -417,7 +441,7 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "YearBook/index.html#-13",
+    "location": "YearBook/index.html#-16",
     "page": "历史年鉴",
     "title": "2014",
     "category": "section",
@@ -425,7 +449,15 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "YearBook/index.html#-14",
+    "location": "YearBook/index.html#-17",
+    "page": "历史年鉴",
+    "title": "2015",
+    "category": "section",
+    "text": "11.7 两岸领导人于新加坡香格里拉大酒店举行会晤，这是两岸分治以来首次领导人直接交流"
+},
+
+{
+    "location": "YearBook/index.html#-18",
     "page": "历史年鉴",
     "title": "2016",
     "category": "section",
@@ -433,7 +465,7 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "YearBook/index.html#-15",
+    "location": "YearBook/index.html#-19",
     "page": "历史年鉴",
     "title": "2017",
     "category": "section",
@@ -441,11 +473,43 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "YearBook/index.html#-16",
+    "location": "YearBook/index.html#-20",
     "page": "历史年鉴",
     "title": "2018",
     "category": "section",
     "text": "5.1 多米尼加与民国断交\n5.24 布基纳法索与民国断交\n8.21 萨尔瓦多与民国断交\n8.24 澳大利亚总理特恩布尔下台，国防部长斯科特·莫里森（Scott Morrison）出任新任总理"
+},
+
+{
+    "location": "YearBook/peoples.html#",
+    "page": "人物志",
+    "title": "人物志",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "YearBook/peoples.html#人物志-1",
+    "page": "人物志",
+    "title": "人物志",
+    "category": "section",
+    "text": "多采自维基百科"
+},
+
+{
+    "location": "YearBook/peoples.html#台湾省-1",
+    "page": "人物志",
+    "title": "台湾省",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "YearBook/peoples.html#马英九-1",
+    "page": "人物志",
+    "title": "马英九",
+    "category": "section",
+    "text": "马英九（1950.7.13-），生于香港九龙油麻地，祖籍湖南衡山县。1952年定居台北。 毕业于台大法律系，后获得哈佛大学司法学博士学位。 曾任蒋经国英文翻译。 1993年受行政院长连战之邀出任法务部长。 1998年击败陈水扁任台北市市长。 2005年当选国民党党主席。 2008年当选台湾省省长。 2012年连任。 2016年卸任。"
 },
 
 ]}
