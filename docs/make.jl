bda08641c1f247595863c3dd52157a9c811023c3
liuxu <liuxu89@mail.ustc.edu.cn>
2020-01-28 16:40:50 +0800

    update travis

--- docs/make.jl
+++ docs/make.jl
@@ -5,38 +5,38 @@ makedocs(
     format = Documenter.HTML( assets = [ "assets/favicon.ico", ] ), #"custom.css", ] ),
     pages  = [
         "Home" => "index.md",
-        "Compute Science Book" => Any[
-            "Advanced Programming in the UNIX Environment" => Any[
-                "CS-books/APUE/index.md"],
-            ],
-        "Algorithm" => Any[
-            "Algorithm/autodiff.md",
-            "LeetCode" => Any[
-                "LeetCode/twostring.md",
-                "LeetCode/palindrome.md",
-                "LeetCode/array.md",
-                "LeetCode/hash.md",
-                "LeetCode/heap.md",
-                ],
-            ],
-        "Languages" => Any[
-            "Python"  => Any[ "Python/index.md",  ],
-            "Fortran" => Any[ "Fortran/Qsort.md", ],
-            ],
-        "系统运维" => Any[
-            "Ops/index.md",
-            "Ops/git-summary.md",
-            ],
-        "Digest" => Any[
-            "Digest/index.md",
-            ],
-        "年鉴" => Any[
-            "YearBook/index.md",
-            "YearBook/peoples.md",
-            ],
-        "百科杂记" => Any["wiki/wiki.md",
-            ],
-        "English.md",
+        #  "Compute Science Book" => Any[
+        #      "Advanced Programming in the UNIX Environment" => Any[
+        #          "CS-books/APUE/index.md"],
+        #      ],
+        #  "Algorithm" => Any[
+        #      "Algorithm/autodiff.md",
+        #      "LeetCode" => Any[
+        #          "LeetCode/twostring.md",
+        #          "LeetCode/palindrome.md",
+        #          "LeetCode/array.md",
+        #          "LeetCode/hash.md",
+        #          "LeetCode/heap.md",
+        #          ],
+        #      ],
+        #  "Languages" => Any[
+        #      "Python"  => Any[ "Python/index.md",  ],
+        #      "Fortran" => Any[ "Fortran/Qsort.md", ],
+        #      ],
+        #  "系统运维" => Any[
+        #      "Ops/index.md",
+        #      "Ops/git-summary.md",
+        #      ],
+        #  "Digest" => Any[
+        #      "Digest/index.md",
+        #      ],
+        #  "年鉴" => Any[
+        #      "YearBook/index.md",
+        #      "YearBook/peoples.md",
+        #      ],
+        #  "百科杂记" => Any["wiki/wiki.md",
+        #      ],
+        #  "English.md",
     ],
     repo     = "https://github.com/auxetic/wiki/blob/{commit}{path}#L{line}",
     #  assets   = ["assets/custom.css", "assets/custom.js", "assets/favicon.ico"],
